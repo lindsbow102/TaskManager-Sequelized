c1c3cf9557378211c95a878412b8de4344eeae99
Lindsey Bowen <lindsbow102@yahoo.com>
2019-04-07 12:20:47 -0700

    Sequelized files and model

--- models/task.js
+++ models/task.js
@@ -1,29 +1,10 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
-
-var task = {
-  selectAll: function(cb) {
-    orm.selectAll("tasks", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("tasks", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("tasks", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("tasks", condition, function(res) {
-      cb(res);
-    });
-  }
-};
-
-// Export the database functions for the controller (tasksController.js).
-module.exports = task;
+module.exports = function(sequelize, DataTypes) {
+  var Task = sequelize.define("Task", {
+    task_name: DataTypes.STRING,
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return Task;
+}
