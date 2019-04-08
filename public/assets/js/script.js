59342434e29ba45d9731e98ee9c667562db0adf4
Lindsey Bowen <lindsbow102@yahoo.com>
2019-04-08 11:39:19 -0700

    Changed POST request syntax

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,28 +1,44 @@
 $(document).ready(function() {
-    
-    // Click function for Changing a task from incomplete to completed
-    $(".complete").on("click", function() {  
-      var task_id = $(this).attr("data-id");
-      console.log("You just clicked this id" + task_id);
-      $.ajax({
-        method: "PUT",
-        url: "/tasks/" + task_id
-      }).then(function(data) {
-        // reload page to display completed task in proper column
-        location.reload();
-      });  
-    });
+  // Click event for Posting a new task
+  $("#submit-task").on("click", function(e) {
+    e.preventDefault();
 
-    // Click function for Deleting a completed task
-    $(".delete-me").on("click", function() {  
-        var task_id = $(this).attr("data-id");
-        console.log("You just clicked this id" + task_id);
-        $.ajax({
-          method: "DELETE",
-          url: "/tasks/delete/" + task_id
-        }).then(function(data) {
-          // reload page to empty out newly deleted task
-          location.reload();
-        });  
-      });
+    const newTask = $("#new-task").val().trim();
+    console.log(newTask);
+
+    $.ajax("/tasks/create", {
+      type: "POST",
+      data: {
+        task_name: newTask
+      }
+    }).then(function() {
+      location.reload();
+    });
   });
+
+  // Click event for Updating a task from incomplete to completed
+  $(".complete").on("click", function() {
+    var task_id = $(this).attr("data-id");
+    console.log("You just clicked this id" + task_id);
+    $.ajax({
+      method: "PUT",
+      url: "/tasks/" + task_id
+    }).then(function(data) {
+      // reload page to display completed task in proper column
+      location.reload();
+    });
+  });
+
+  // Click event for Deleting a completed task
+  $(".delete-me").on("click", function() {
+    var task_id = $(this).attr("data-id");
+    console.log("You just clicked this id" + task_id);
+    $.ajax({
+      method: "DELETE",
+      url: "/tasks/delete/" + task_id
+    }).then(function(data) {
+      // reload page to empty out newly deleted task
+      location.reload();
+    });
+  });
+});
